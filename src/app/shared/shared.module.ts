0aec759d3dfeb5369e93d61dd0c0873a4b171cc0
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-11-26 10:49:11 -0500

    update - finished 'shell' feature module etc.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,25 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { ShellComponent } from './shell/shell.component';
 
 const components = [ShellComponent];
-const modules = [MatButtonModule];
+const modules = [
+  CommonModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  LayoutModule,
+  MatMenuModule,
+];
 
 @NgModule({
   declarations: [components],

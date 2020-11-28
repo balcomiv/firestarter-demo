49440017b66005e6d2c833a0a2cf8908232cf9eb
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-11-28 12:37:09 -0500

    update - email login

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,14 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { ShellComponent } from './shell/shell.component';
@@ -14,13 +18,18 @@ const components = [ShellComponent];
 const modules = [
   CommonModule,
   RouterModule,
-  MatButtonModule,
   MatToolbarModule,
   MatIconModule,
+  LayoutModule,
+  MatButtonModule,
   MatSidenavModule,
   MatListModule,
-  LayoutModule,
   MatMenuModule,
+  MatIconModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSnackBarModule,
 ];
 
 @NgModule({
--- src/app/user/components/email-login/email-login.component.ts
+++ src/app/user/components/email-login/email-login.component.ts
@@ -1,15 +1,94 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+type LoginType = 'login' | 'signup' | 'reset';
 
 @Component({
   selector: 'app-email-login',
   templateUrl: './email-login.component.html',
-  styleUrls: ['./email-login.component.scss']
+  styleUrls: ['./email-login.component.scss'],
 })
 export class EmailLoginComponent implements OnInit {
+  form!: FormGroup;
+  loginType: LoginType = 'login';
+  loading = false;
+  serverMessage = '';
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordConfirm: [''],
+    });
   }
 
+  changeType(loginType: LoginType): void {
+    this.loginType = loginType;
+  }
+
+  get isLogin(): boolean {
+    return this.loginType === 'login';
+  }
+
+  get isSignup(): boolean {
+    return this.loginType === 'signup';
+  }
+
+  get isPasswordReset(): boolean {
+    return this.loginType === 'reset';
+  }
+
+  get email(): AbstractControl | null {
+    return this.form.get('email');
+  }
+
+  get password(): AbstractControl | null {
+    return this.form.get('password');
+  }
+
+  get passwordConfirm(): AbstractControl | null {
+    return this.form.get('passwordConfirm');
+  }
+
+  get signup(): AbstractControl | null {
+    return this.form.get('signup');
+  }
+
+  get passwordDoesMatch(): boolean {
+    if (this.loginType !== 'signup') {
+      return true;
+    }
+    return this.password?.value === this.passwordConfirm?.value;
+  }
+
+  async onSubmit(): Promise<void> {
+    this.loading = true;
+
+    const email = this.email?.value;
+    const password = this.password?.value;
+
+    try {
+      if (this.isLogin) {
+        await this.afAuth.signInWithEmailAndPassword(email, password);
+      }
+      if (this.isSignup) {
+        await this.afAuth.createUserWithEmailAndPassword(email, password);
+      }
+      if (this.isPasswordReset) {
+        await this.afAuth.sendPasswordResetEmail(email);
+      }
+    } catch (error) {
+      this.serverMessage = error;
+    }
+
+    this.loading = false;
+  }
 }

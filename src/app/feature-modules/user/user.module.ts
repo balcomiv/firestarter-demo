2fcb76006d2db3346ef5510b252e164e7f10b27a
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-11-28 07:45:37 -0500

    add - email login

--- src/app/feature-modules/user/user.module.ts
+++ src/app/feature-modules/user/user.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { GoogleSigninDirective } from './directives/google-signin.directive';
+import { UserRoutingModule } from './user-routing.module';
+import { EmailLoginComponent } from './components/email-login/email-login.component';
 
 @NgModule({
-  declarations: [LoginComponent, GoogleSigninDirective],
-  imports: [CommonModule, UserRoutingModule, SharedModule],
+  declarations: [LoginComponent, GoogleSigninDirective, EmailLoginComponent],
+  imports: [CommonModule, ReactiveFormsModule, UserRoutingModule, SharedModule],
 })
 export class UserModule {}

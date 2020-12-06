1cc34507dd89e9cc1b36d2ac0d99f29f92e1484d
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-12-06 09:55:00 -0500

    update - board list

--- src/app/kanban/components/board-list/board-list.component.ts
+++ src/app/kanban/components/board-list/board-list.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Board } from '../../board.interface';
+import { BoardService } from '../../services/board.service';
 
 @Component({
   selector: 'app-board-list',
   templateUrl: './board-list.component.html',
-  styleUrls: ['./board-list.component.scss']
+  styleUrls: ['./board-list.component.scss'],
 })
-export class BoardListComponent implements OnInit {
+export class BoardListComponent implements OnInit, OnDestroy {
+  boards?: Board[];
+  sub?: Subscription;
 
-  constructor() { }
+  constructor(private boardService: BoardService) {}
 
   ngOnInit(): void {
+    this.sub = this.boardService
+      .getUserBoards()
+      .subscribe((boards) => (this.boards = boards));
   }
 
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+
+  /**
+   * Handles Cdk Drag and Drop Event
+   * @param event Cdk Drag/Drop Event
+   */
+  drop(event: CdkDragDrop<string[]>): void {
+    if (!this.boards) {
+      console.error(`Boards not set!`);
+      return;
+    }
+
+    moveItemInArray(this.boards, event.previousIndex, event.currentIndex);
+    this.boardService.sortBoards(this.boards);
+  }
 }

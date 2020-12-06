98dcb39a18b22019a5edda6e38c74aeb0607666a
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-12-06 09:20:59 -0500

    add - board && board list components

--- src/app/kanban/kanban.module.ts
+++ src/app/kanban/kanban.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatDialogModule } from '@angular/material/dialog';
 import { SharedModule } from '../shared/shared.module';
+import { BoardListComponent } from './components/board-list/board-list.component';
+import { BoardComponent } from './components/board/board.component';
 import { KanbanRoutingModule } from './kanban-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [BoardComponent, BoardListComponent],
   imports: [
     CommonModule,
     KanbanRoutingModule,
